be9e12343b997a46e65aded72d07e441b4a59d9d
josenauto <jemilionautoch@gmail.com>
2020-12-16 15:10:53 -0300

    Modified the component tours

---
+++ src/Tours.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Tour from "./Tour";
+
+const Tours = () => {
+  return (
+    <div>
+      <h2>tours component</h2>
+    </div>
+  );
+};
+
+export default Tours;
