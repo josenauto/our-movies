aeb9065551c3bdc868a790cf34f5d080cf28df3a
josenauto <jemilionautoch@gmail.com>
2020-12-16 18:21:27 -0300

    Implemented the read more functionality

--- src/Tour.js
+++ src/Tour.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <div>
       <img src={image} alt={name} height="400" width="650" />
       <h4>{name}</h4>
       <h4>${price}</h4>
-      <p>{info}</p>
+      <p>
+        {readMore ? info : `${info.substring(9, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "show less" : "read more"}
+        </button>
+      </p>
       <button>not interested</button>
     </div>
   );
